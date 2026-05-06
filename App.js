36a20fd848e667616eea4493dce11d491c4c6faa
NicoBadenes <nicolasbadenes@gmail.com>
2026-05-06 11:50:05 -0300

    division y lista de pokemones

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useEffect } from 'react';
+import { FlatList } from 'react-native';
+
 
 export default function App() {
+//Hook de react usestate (estado)
+const [pokemon, setPokemon] = useState([]);
+
+//funcion para llamar una API
+//fetch
+//async await
+
+const getPokemon = async () => {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+    const data = await response.json();
+    console.log("Pokemon", data);   
+    setPokemon(data.results);
+
+} catch (error) {
+    console.error("ERROR: No fue posible obtener los pokemones", error);
+}
+};
+
+useEffect(() => {
+  getPokemon();
+}, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.list}>
+          <Text>Pokedex</Text>
+          <Text>Pokémon List</Text>
+          <FlatList
+            data={pokemon}
+            renderItem={({item}) => <Text>{item.name}</Text>}
+            keyExtractor={item => item.name}
+          />
+        </View>
+        <View style={styles.details}></View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  list: {
+    flex: 0.5,
+    backgroundColor: "red",
+  },
+  details: {
+    flex: 0.5,
+    backgroundColor: "green",
+  }
 });
+
